89bb0d4419c598601f6bd254227fe0bad5c9b08d
Piter Capistrano <pitercapistrano@gmail.com>
2024-07-19 16:56:19 -0300

    Aulas de Anatomia das Classes concluidas

--- 1-java-anatomia-classes/src/edu/piter/aula1java/MinhaClass.java
+++ 1-java-anatomia-classes/src/edu/piter/aula1java/MinhaClass.java
@@ -1,4 +1,4 @@
-package edu.piter.java;
+package edu.piter.aula1java;
 //!     ANATOMIA DAS CLASSES
 
    /* Uma classe bem estruturada não quer guerra com ninguém
@@ -412,6 +412,56 @@ nomenclatura dos pacotes, logo abaixo nos exemplos:
     !   "InvalidOrderException", etc...
 
         Organizar o código em pacotes seguindo essas convenções ajuda a manter a estrutura do projeto limpa e
-        compreensível, facilita a navegação e a manutenção, e promove boas práticas de desenvolvimento. Cada
-        pacote tem um propósito claro, tornando mais fácil localizar e entender diferentes partes do código. 
+    compreensível, facilita a navegação e a manutenção, e promove boas práticas de desenvolvimento. Cada
+    pacote tem um propósito claro, tornando mais fácil localizar e entender diferentes partes do código. 
   */
+
+  /*
+   !        JAVA BEANS
+        Uma das maiores dificuldades na programação é escrever algoritmos legíveis a níveis que sejam compreendidos
+    por todo seu time ou por você mesmo no futuro. Para isso a linguagem Java sugere, através de convenções,
+    formas de escrita universal para nossas classes, atributos, métodos e pacotes.
+
+    ?       Variáveis:
+
+        Mais cedo já aprendemos algumas regras de declaração de variáveis, mas agora iremos conhecer algumas sugestões
+    de nomenclatura:
+
+    - Uma variável deve ser clara, sem abreviações ou definição sem sentido;
+
+    - Uma variável é sempre no singular, exceto quando se referir a um array ou coleção;
+
+    - Defina um idioma único para suas variáveis. Se você for declarar variáveis em inglês, defina todas em inglês.
+   
+    ?       Não recomendado:
+
+        double salMedio = 1500.23 - variável abreviada;
+        String emails = "aluno@escola.com" - confuso se a variável é apenas de um email ela deve ser no singular;
+        String myName = "Piter" - se idioma pt-BR, as variáveis devem ser escritas em pt-BR.
+
+
+    ?      Recomendado:
+
+        double salarioMedio = 1500.23;
+        String email = "aluno@escola.com";
+        String [] emails = {"aluno@escola.com","professor@escola.com",...};
+        String meuNome = "Piter";
+
+    ?      Métodos:
+        
+        Os métodos deverão ser nomeados como verbos, através de uma mistura de letras minúsculas e maiusculas.
+    Em princípio todas as letras que compõem o nome devem ser mantidas em minúsculo, com exceção da primeira letra
+    de cada palavra composta a partir da segunda palavra.
+
+    ?      Exemplos sugeridos para nomenclatura de métodos:
+
+        somar(int n1, int n2){}
+
+        abrirConexao(){}
+
+        concluirProcessamento(){}
+
+        findById (int id) {} - não se assuste, você verá muitos métodos em inglês em sua jornada.
+
+        calcularImprimir(){} - há algo de errado neste método, ele deveria ter uma única finalidade, calcular ou imprir
+    */
