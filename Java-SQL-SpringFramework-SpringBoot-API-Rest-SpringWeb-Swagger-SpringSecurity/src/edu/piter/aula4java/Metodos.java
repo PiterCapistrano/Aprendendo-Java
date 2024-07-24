bfcfb15608cf1cfbf47582be0191a6445be27332
Piter Capistrano <pitercapistrano@gmail.com>
2024-07-24 10:34:56 -0300

    Aula Sobre Métodos

--- Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/piter/aula4java/Metodos.java
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/piter/aula4java/Metodos.java
@@ -5,7 +5,11 @@ public class Metodos {
 /*
 !                   MÉTODOS
 
-    
+?        Todas as ações das aplicações são consideradas métodos
+
+      Uma classe é definida por atributos e métodos. Já vimos que atributos são, em sua grande maioria, variáveis
+    de diferentes tipos e valores. Os métodos, por sua vez, correspondem a funções ou sub-rotinas disponíveis
+    dentro de nossas classes.
 */    
     
   }  
