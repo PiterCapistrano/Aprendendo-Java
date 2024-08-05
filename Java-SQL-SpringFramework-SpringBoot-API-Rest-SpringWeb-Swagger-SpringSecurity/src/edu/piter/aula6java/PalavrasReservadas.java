519b243df4f193b9fe37ce6c666e290722b1e2d7
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-05 10:04:07 -0300

    Aula Sobre Palavras Reservadas

---
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/piter/aula6java/PalavrasReservadas.java
@@ -0,0 +1,12 @@
+package edu.piter.aula6java;
+
+public class PalavrasReservadas {
+/*
+!         PALAVRAS RESERVADAS
+
+?       Objetivo da Aula:
+
+      Apresentar as 52 palavras reservadas organizadas por classificação de usabilidade considerando as regras da
+    linguagem.
+*/
+}
