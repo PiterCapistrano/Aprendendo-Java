f276d9c6c4a14d82c830ad7a508840787fcfb195
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-05 18:55:09 -0300

    Aula Sobre Palavras Reservadas

--- Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/piter/aula6java/PalavrasReservadas.java
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/piter/aula6java/PalavrasReservadas.java
@@ -6,7 +6,63 @@ public class PalavrasReservadas {
 
 ?       Objetivo da Aula:
 
-      Apresentar as 52 palavras reservadas organizadas por classificação de usabilidade considerando as regras da
-    linguagem.
+    Apresentar as 52 palavras reservadas organizadas por classificação de usabilidade considerando as regras da
+  linguagem.
+
+?       Sobre Palavras reservadas:
+
+    Palavras reservadas são identificadores de uma linguagem que já possuem uma finalidade específica, portanto
+  não podem ser utilizados para nomear variáveis, classes, métodos ou atributos.
+
+    A linguagem Java possui 52 palavras reservadas. Todas essas palavras são classificadas em grupos e escritas
+  com letra minúscula, sendo identificadas com uma cor especial pela maioria das IDE's. Abaixo temos a lista de
+  palavras agrupadas por suas finalidades.
+
+!         PALAVRAS RESERVADAS EM JAVA
+
+?       Controle de Pacotes:
+
+    - "import": importa pacotes ou classes para dentro do código.
+    Exemplo: import java.util.ArrayList;
+
+    - "package": especifica a que pacote todas as classes de um arquivo pertencem.
+    Exemplo: package com.example;
+
+?       Modificadores de Acesso:
+
+    - "public": acesso de qualquer classe. Deixa a minha classe, o meu atributo e meu método público, sem restrições.
+    Exemplo: public String name;
+
+    - "private": acesso apenas dentro da classe.
+    Exemplo: private String secret;
+
+    - "protected": acesso por classes no mesmo pacote e subclasses. Tem como finalidade ser trabalhada com herança.
+    Exemplo: protected String familySecret;
+
+?      Primitivos:
+
+    - "boolean": um valor indicando verdadeiro(true) ou falso(false).
+    Exemplo: boolean isJavaFun = true;
+
+    - "byte": um inteiro de 8 bits (signed).
+    Exemplo: byte b = 100;
+
+    - "char": um character unicode (16-bit unsigned).
+    Exemplo: char letter = 'A';
+
+    - "double": um número de ponto flutuante de 64 bits (signed).
+    Exemplo: double pi = 3.14159;
+
+    - "float": um número de ponto flutuante de 32 bits (signed).
+    Exemplo: float pi = 3.14f;
+
+    - "int": um inteiro de 32 bits (signed).
+    Exemplo: int number = 100;
+
+    - "long": um inteiro de 64 bits (signed).
+
+    - "short": um inteiro de 32 bits (signed).
+
+    - "void": indica que o método não tem retorno de valor
 */
 }
