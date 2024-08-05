5e3cf6bf8735d9008f420e920a1a4e8d5c8e0b62
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-04 23:54:53 -0300

    Aula Sobre Métodos Completa

---
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/piter/aula4java/SmartTv.java
@@ -0,0 +1,53 @@
+package edu.piter.aula4java;
+/*
+!       EXERCICIO PRÁTICO:
+
+    Vamos criar um exemplo de uma classe para representar uma SmartTV onde:
+
+    1 - Ela tenha as características: ligada (boolean), canal (int) e volume (int).
+
+    2 - Nossa TV poderá ligar e desligar e assim mudar o estado ligada.
+
+    3 - Nossa TV aumentará e diminuirá o volume sem em +1 ou -1.
+
+    4 - Nossa TV poderá mudar de canal de 1 em 1 ou definido o número correspondente.
+*/
+
+public class SmartTv {
+  boolean ligada = false;
+  int canal = 1;
+  int volume = 25;
+
+  public void ligar(){
+    ligada = true;
+  }
+
+  public void desligar(){
+    ligada = false;
+  }
+
+  public void aumentarVolume(){
+    volume++;
+    System.out.println("Aumentando o volume para = " + volume);
+  }
+
+  public void diminuirVolume(){
+     volume--;
+     System.out.println("Diminuindo o volume para = " + volume);
+  }
+
+  public void aumentarCanal(){
+    canal++;
+    System.out.println("Aumentou o canal para = " + canal);
+  }
+
+  public void diminuirCanal(){
+    canal--;
+    System.out.println("Diminuiu o canal para = " + canal);
+  }
+
+  public void mudarDeCanal(int novoCanal){
+    canal = novoCanal;
+    System.out.println("Mudou o canal para = " + canal);
+  }
+} 
---
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/piter/aula4java/UsuarioDaSmartTv.java
@@ -0,0 +1,41 @@
+package edu.piter.aula4java;
+
+public class UsuarioDaSmartTv {
+  public static void main(String[] args) throws Exception {
+    
+    SmartTv smartTv = new SmartTv(); // Código cria uma instância da classe SmartTv
+
+    if (smartTv.ligada == false) {  // Se a TV estiver desligada esse comando irá ligar ela.
+      smartTv.ligar();
+      System.out.println("A TV foi ligada " + smartTv.ligada);
+
+    } else // Caso ela já esteja ligada ele irá executar esses comandos diretamente, sem precisar apertar o botão de ligar a TV.
+    
+    System.out.println("TV ligada " + smartTv.ligada);
+    System.out.println("Canal atual = " + smartTv.canal);
+    System.out.println("Volume atual = " + smartTv.volume);
+
+    smartTv.diminuirVolume();
+    smartTv.diminuirVolume();
+    smartTv.diminuirVolume();
+    System.out.println("Volume diminuido para = " + smartTv.volume);
+
+    smartTv.aumentarVolume();
+    smartTv.aumentarVolume();
+    System.out.println("Volume aumentado para = " + smartTv.volume);
+
+    smartTv.aumentarCanal();
+    smartTv.aumentarCanal();
+    System.out.println("Canal atual = " + smartTv.canal);
+
+    smartTv.diminuirCanal();
+    System.out.println("Canal atual = " + smartTv.canal);
+
+    smartTv.mudarDeCanal(125);
+    System.out.println("Canal atual = " + smartTv.canal);
+
+    smartTv.desligar();
+    System.out.println(smartTv.ligada);
+    System.out.println("A TV foi desligada!");
+  }
+}
