0cbf9d84167a52711c865afc6744f5b415edc0a3
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-06 09:35:44 -0300

    Aula Sobre Java Documentation

--- Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/piter/aula7java/JavaDoc.java
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/piter/aula7java/JavaDoc.java
@@ -8,6 +8,35 @@ public class JavaDoc {
 
     Apresentar a ferramenta de documentaçao da linguagem e explorar como adicionar comentários em nossos arquivos.
 
-?
+?     Documentação:
+
+    Uma das características da linguagem Java é que, desde suas primeiras versões, tinhamos em nossas mãos, uma
+  documentação rica e detalhada dos recursos da linguagem.
+
+    Conforme site oficial, podemos compreender e explorar, todos os recursos organizados por pacotes e classes
+  bem específicas, sem nem mesmo escrever uma linha de código.
+
+    Hoje, costuma-se afirmar que, para se tornar um desenvolvedor nível avançado, é um requisito imprescindível
+  adquirir a habilidade de compreender, a documentação oficial da linguagem e dos frameworks que são incorporados
+  nos projetos atuais.
+
+?       Tags:
+
+    Mas e quais as informações, que obtemos através de classes documentadas, na linguagem? Java Documentation é
+  composto por tags que, representam dados relevantes para a compreensão da proposta de uma classe e os conjuntos
+  de seus métodos e atributos conforme tabela abaixo:
+
+?      Tag                              Descrição
+     @autor                          Autor / Criador
+
+     @version                        Versão do recurso disponibilizado
+
+     @since                          Versão / Data de início da disponibilização do recurso
+
+     @param                          Descrição dos parâmetros dos métodos criados
+
+     @return                         Definição do tipo de retorno de um método
+
+     @throws                         Se o método lança alduma exceção
 */
 }
