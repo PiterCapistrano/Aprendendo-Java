74131bab8b39060fd36e81dd220c1f593979abe6
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-06 20:18:16 -0300

    Aula Sobre Java Documentation

--- Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/piter/aula7java/JavaDoc.java
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/piter/aula7java/JavaDoc.java
@@ -71,3 +71,21 @@ public class JavaDoc {
         System.out.println(calculadora.somar(23, 27)); // Saída: 50
     }
  }
+ /*
+?       Tipos de Comentários:
+*/
+// Olá, eu sou um comentário em uma única linha.
+
+/* Olá,
+ * Eu sou um comentário
+ * que posso ser mais detalhado
+ * quando necessário
+ */
+
+/**
+ * Estas duas estrelinhas acima
+ * é para identificar que você
+ * pretende elaborar um comentário
+ * a nível de documentação.
+ * Que incrível !!!
+ */
