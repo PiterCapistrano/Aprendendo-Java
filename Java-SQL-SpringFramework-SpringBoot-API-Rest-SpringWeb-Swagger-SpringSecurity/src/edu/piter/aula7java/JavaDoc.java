0a47b145bd655d5513ef183539bd7ba0c9e4fe85
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-06 20:35:16 -0300

    Aula Sobre Java Documentation

--- Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/piter/aula7java/JavaDoc.java
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/piter/aula7java/JavaDoc.java
@@ -97,4 +97,23 @@ public class JavaDoc {
 
 /*
  * Este método foi elaborado as pressas
+ * por isso eu abreviei o nome das variáveis
+ * mas olha, ele tem a finalidade de somar ou multiplicar
+ * dois números
  */
+class SomaMultiplica {
+    public int SomaMultiplica(int n, int x, String m){
+        int r = 0; // r é igual ao resultado
+        if (m == "M") { // M= multiplicação
+            r= n * x;
+        }else{
+            // se não soma mesmo
+            r = n + x;
+        }
+        return r;
+    }
+    public static void main(String[] args) {
+        SomaMultiplica somaMultiplica = new SomaMultiplica();
+        System.out.println(somaMultiplica.SomaMultiplica(20, 10, "M"));
+    }
+}
