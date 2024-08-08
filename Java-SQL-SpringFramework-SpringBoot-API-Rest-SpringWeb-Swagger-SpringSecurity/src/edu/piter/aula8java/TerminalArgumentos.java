83da92855bcf01084a18d4d5aeca50286ea22b2a
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-08 20:54:28 -0300

    Aula Sobre Terminal e Argumentos Completa

--- Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/piter/aula8java/TerminalArgumentos.java
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/piter/aula8java/TerminalArgumentos.java
@@ -87,5 +87,42 @@ public class AboutMe {
         cd bin
 
         java AboutMe nome sobrenome idade altura
+
+?       Scanner:
+
+    Nos exemplos anteriores, percebemos que podemos receber, dados digitados pelo usuário do nosso sistema, porém,
+  tudo precisa estar em uma linha e também é necessário informar os valores nas posições correspondentes. Esta
+  abordagem pode deixar margens de execução, com erro do nosso programa. Para issa, com a finalidade de deixar as
+  nossas entradas de dados mais seguras, agora vamos receber estes dados via Scanner.
+
+    A classe Scanner, permite que o usuário tenha, uma interação mais assertiva com o nosso programa, veja como
+  vamos mudar o nosso programa AboutMe para deixar mais intuitivo aos usários:
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe2 {
+    public static void main(String[] args) {
+        // criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        // Imprimindo os dados obtidos pelo usuario
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+    }
+}
 */ 
 }
