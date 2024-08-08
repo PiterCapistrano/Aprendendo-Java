86a8664fe4be86ecafb3acb8b5ebf9d2d075bf49
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-08 19:08:08 -0300

    Aula Sobre Terminal e Argumentos

--- Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/piter/aula8java/TerminalArgumentos.java
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/piter/aula8java/TerminalArgumentos.java
@@ -1,5 +1,24 @@
 package edu.piter.aula8java;
 
 public class TerminalArgumentos {
-  
+/*
+!         TERMINAL E ARGUMENTOS
+
+?       Objetivo da Aula:
+
+    Explorar opções de entrada e saída simples de dados em uma aplicação em Java.
+
+?       Introdução;
+
+    Nem sempre executamos nosso programa Java pela IDE, já pensou, nossos clientes tendo que instalar o Eclipse
+  ou VsCode para rodar o sistema em sua empresa?
+
+    Com a JVM devidamente configurada, nós podemos criar um executável do nosso programa e disponibilizar o 
+  instalador para qualquer sistema operacional.
+
+    No nosso caso, iremos aprender como executar um programa Java via terminal, como MS - DOS ou terminal do 
+  VsCode.
+
+    Vamos criar uma classe chamada MinhaClasse.java com o código abaixo:
+*/ 
 }
