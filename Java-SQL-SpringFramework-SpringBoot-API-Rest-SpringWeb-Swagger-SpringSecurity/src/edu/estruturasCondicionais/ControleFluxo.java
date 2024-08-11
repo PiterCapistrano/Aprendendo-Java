9d4cf5b59715b2475a8034da54cfae27f27fcaa8
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-10 21:36:46 -0300

    Aula Sobre Controle de Fluxos

---
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasCondicionais/ControleFluxo.java
@@ -0,0 +1,25 @@
+package edu.estruturasCondicionais;
+
+public class ControleFluxo {
+/*
+!         OBJETIVO DA AULA
+
+    Apresentar os conceitos e explorar os tipos de fluxos condicional, excepcional e de repetição em uma aplicação
+  Java.
+
+?       Assutos:
+
+    - Estruturas condicionais: if-else, switch-case
+
+    - Estruturas de repetição: for, while, do-while
+
+    - Estruturas de exceções: try-catch-finally, throw
+
+!         INTRODUÇÃO AO CONTROLE DE FLUXO
+
+    Controle de fluxo é a habilidade de ajustar a maneira como um programa realiza suas tarefas. Por meio de 
+  instruções especiais, chamadas de comandos, essas tarefas podem ser executadas seletivamente, condicionalmente, 
+  repetidamente ou excepcionalmente.
+
+ */
+}
