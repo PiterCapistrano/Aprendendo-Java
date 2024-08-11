2e06148d3ef523eb6178078f987236cb31200828
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-10 22:30:15 -0300

    Aula Sobre Estrutura Condicional Simples

--- Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasCondicionais/EstruturaCondicionalSimples.java
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasCondicionais/EstruturaCondicionalSimples.java
@@ -3,5 +3,8 @@ package edu.estruturasCondicionais;
 public class EstruturaCondicionalSimples {
 /*
 !         ESTRUTURAS CONDICIONAIS
+
+    A Estrutura Condicional possibilita a escolha de um grupo de ações e comportamentos a serem executadas quando
+  determinadas condições são ou não satisfeitas. A Estrutura Condicional pode ser Simples ou Composta.
  */
 }
