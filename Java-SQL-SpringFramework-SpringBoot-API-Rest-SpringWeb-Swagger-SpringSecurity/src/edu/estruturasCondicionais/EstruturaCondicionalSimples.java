4118b39011c1e6cfc1401ffdda6cc0fbf9f04c1b
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-12 10:04:19 -0300

    Aula Sobre Estrutura Condicional Simples

--- Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasCondicionais/EstruturaCondicionalSimples.java
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasCondicionais/EstruturaCondicionalSimples.java
@@ -6,5 +6,23 @@ public class EstruturaCondicionalSimples {
 
     A Estrutura Condicional possibilita a escolha de um grupo de ações e comportamentos a serem executadas quando
   determinadas condições são ou não satisfeitas. A Estrutura Condicional pode ser Simples ou Composta.
+
+!         CONDICIONAIS SIMPLES
+
+    Quando ocorre uma validação de execução de fluxo somente quando a condição for positiva, consideramos como uma
+  estrutura Simples, exemplo:
  */
+}
+class CaixaEletronico {
+  public static void main(String[] args) {
+    
+    double saldo = 25.0;
+    double valorSolicidado = 17.0;
+
+    if (valorSolicidado < saldo) { 
+      saldo = saldo - valorSolicidado;
+
+      System.out.println("Saldo = "+saldo);
+    }
+  }
 }
