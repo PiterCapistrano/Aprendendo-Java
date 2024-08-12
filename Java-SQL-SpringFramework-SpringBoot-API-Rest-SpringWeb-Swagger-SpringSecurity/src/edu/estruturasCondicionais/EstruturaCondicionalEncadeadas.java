36a85698f0b56fd8e8530bed88458f0b453a60a5
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-12 10:20:32 -0300

    Aula Sobre Estrutura Condicional Simples

---
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasCondicionais/EstruturaCondicionalEncadeadas.java
@@ -0,0 +1,10 @@
+package edu.estruturasCondicionais;
+
+public class EstruturaCondicionalEncadeadas {
+/*
+!       CONDICIONAIS ENCADEADAS
+
+    EM um controle de fluxo condicional, nem sempre nos limitamos ao se (if) e senão(else), podemos ter uma terceira,
+  quarta e ou inúmeras condições.
+ */
+}
