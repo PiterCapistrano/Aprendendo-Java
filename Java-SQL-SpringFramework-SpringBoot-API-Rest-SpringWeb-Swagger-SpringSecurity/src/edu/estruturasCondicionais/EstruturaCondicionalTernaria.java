ba0dacecf5e3e4242bf756faf2ca1fef202e9b7a
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-12 18:20:05 -0300

    Aula Sobre Estrutura Condicional

--- Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasCondicionais/EstruturaCondicionalTernaria.java
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasCondicionais/EstruturaCondicionalTernaria.java
@@ -1,5 +1,18 @@
 package edu.estruturasCondicionais;
 
 public class EstruturaCondicionalTernaria {
-  
+/*
+!       CONDIÇÃO TERNÁRIA
+
+    Como vimos em operadores, podemos abreviar nosso algorítmo condicional refatorando com o conceito de operador
+  ternário. Vamos refatorar os exemplos acima para ilustrar o poder deste recurso:
+*/
+
+  public static void main(String[] args) {
+    int nota = 7;
+
+    String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+
+    System.out.println(resultado);
+  }
 }
