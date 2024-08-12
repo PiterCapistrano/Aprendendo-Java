6f139c207b0be89097cc00c0d79aa74a8c522c00
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-12 10:13:29 -0300

    Aula Sobre Estrutura Condicional Simples

--- Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasCondicionais/EstruturaCondicionalComposta.java
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasCondicionais/EstruturaCondicionalComposta.java
@@ -1,5 +1,20 @@
 package edu.estruturasCondicionais;
 
 public class EstruturaCondicionalComposta {
+/*
+!       CONDICIONAIS COMPOSTA
+
+    Algumas vezes nosso programa deverá seguir mais de uma jornada de execução condicionado a uma regra de negócio,
+  este cenário é denominado Estrutura Condicional Composta. Vejamos o exemplo abaixo:
+ */
+  public static void main(String[] args) {
   
+  int nota = 6;
+
+    if (nota >= 7) {
+    System.out.println("Aprovado!");
+    } else {
+    System.out.println("Reprovado!");
+    }
+  }
 }
