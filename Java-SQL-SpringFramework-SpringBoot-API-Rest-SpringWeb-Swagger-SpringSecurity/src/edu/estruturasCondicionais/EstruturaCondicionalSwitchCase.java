ece9b4753cfec90abbaf1d1340a523127814d418
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-19 10:23:44 -0300

    Aula Sobre Estrutura Condicional Switch Case

--- Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasCondicionais/EstruturaCondicionalSwitchCase.java
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasCondicionais/EstruturaCondicionalSwitchCase.java
@@ -76,9 +76,45 @@ class SistemaDeMedida {
     
     - TURBO: 100 minutos de ligação + Whats e Instagram grátis + 5Gb Youtube
 */
-
-class PlanoTelefonico {
+//!         CENÁRIO COM IF/ELSE:
+class PlanoTelefonicoIf {
     public static void main(String[] args) {
-      
+      String plano = "M"; // M / T
+
+      if (plano == "B") {
+          System.out.println("100 minutos de ligação");
+      }else if (plano == "M") {
+        System.out.println("100 minutos de ligação");
+        System.out.println("Whats e Instagram grátis");
+      }else if (plano == "T") {
+        System.out.println("100 minutos de ligação");
+        System.out.println("Whats e Instagram grátis");
+        System.out.println("5Gb Youtube");
+      }else
+      System.out.println("Selecione um plano válido: B, M ou T");
     }
   }
+
+//!         CENÁRIO COM SWITCH/CASE:
+
+class PlanoTelefonicoSwitch {
+  public static void main(String[] args) {
+    String plano = "M";
+
+    switch (plano){
+      case "T": {
+        System.out.println("5Gb Youtube");
+      }
+      case "M": {
+        System.out.println("Whats e Instagram grátis");
+      }
+      case "B": {
+        System.out.println("100 minutos de ligação");
+        break;
+      }
+      default:{
+        System.out.println("Selecione um plano válido: B, M ou T");
+      }
+    }
+  }
+}
