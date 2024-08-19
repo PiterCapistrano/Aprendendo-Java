99373a3b16bcd8a55482f4764c3925cfd00a126a
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-19 09:42:03 -0300

    Aula Sobre Estrutura Condicional Switch Case

---
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasCondicionais/EstruturaCondicionalSwitchCase.java
@@ -0,0 +1,30 @@
+package edu.estruturasCondicionais;
+
+public class EstruturaCondicionalSwitchCase {
+/*
+!         SWITCH CASE
+
+    A estrutura "switch" compara o valor de cada caso com o da variável sequencialmente, e sempre que encontra um 
+  valor correspondente, executa o código associado ao caso. Para evitar que as comparações continuem a ser executadas
+  após um caso correspondente ter sido encontrado, acrescentamos o comando "break" no final de cada bloco de códigos.
+  O comando "break", quando executado, encerra a execução da estrutura onde ele se encontra.
+
+    Vamos imaginar que precisaremos imprimir uma medida com base em mapa de valores, exemplo:
+
+  ?      Sigla                                Tamanho
+
+          P                                   Pequeno
+          M                                   Médio
+          G                                   Grande
+ */
+}
+class SistemaMedida {
+  public static void main(String[] args) {
+    String sigla = "M";
+
+    if(sigla == "P"){
+      System.out.println("PEQUENO");
+    }
+      
+  }
+}
