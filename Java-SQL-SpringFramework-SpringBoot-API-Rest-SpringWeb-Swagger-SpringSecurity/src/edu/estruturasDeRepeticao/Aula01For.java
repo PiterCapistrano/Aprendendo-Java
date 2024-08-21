6069919768311ebd1348f517a3b451cefff9082b
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-21 08:33:43 -0300

    Aula Sobre Estrutura de Repetição For

--- Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasDeRepeticao/Aula01For.java
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasDeRepeticao/Aula01For.java
@@ -1,5 +1,10 @@
 package edu.estruturasDeRepeticao;
 
 public class Aula01For {
-  
+/*
+!         ESTRUTURA DE REPETIÇÃO
+
+    Laços de repetição, também conhecidos como laços de iteração ou simplesmente loops, são comandos que permitem
+  iteração de código, ou seja, que comandos presentes no bloco sejam repetidos diversas vezes.
+ */
 }
