8d8f498849d092d2ae341b36ed91a3aa9f80c24a
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-25 20:26:24 -0300

    Aula Sobre Estrutura de Repetição For

--- Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasDeRepeticao/Aula01For.java
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasDeRepeticao/Aula01For.java
@@ -5,6 +5,27 @@ public class Aula01For {
 !         ESTRUTURA DE REPETIÇÃO
 
     Laços de repetição, também conhecidos como laços de iteração ou simplesmente loops, são comandos que permitem
-  iteração de código, ou seja, que comandos presentes no bloco sejam repetidos diversas vezes.
+  iteração de código, ou seja, que comandos presentes no bloco sejam repetidos diversas vezes. Através de laços de
+  repetição é possível criar programas que percorram arrays, analisando individualmente cada elemento, e até mesmo
+  criar trechos de código que sejam repetidos até que certa condição estabelecida seja cumprida.
+
+    Perl possui basicamente quatro tipos de laços de repetição: "while"(enquanto), "do... while"(faça enquanto) 
+  "for"(para) e "foreach"(para cada).
+
+    O comando "for" (na tradução literal para a língua portuguesa "para") permite que uma variável contadora seja
+  testada e incrementada a cada iteração, sendo essas informações definidas na chamada de comando. O comando "for"
+  recebe como entrada uma variável contadora, a condição e o valor de incrementação.
+
+    Uma outra forma de usarmos o laço "for" é declarando uma variável, seguida por dois valores da iteração 
+  (primeiro e último) entre parênteses separados por dois pontos finais.
+
+    A estrutura de sintaxe do controle de repetição "for" é exibida abaixo:
+
+    for (bloco de inicialização; expressão booleana de validação; bloco de atualização)
+    {
+    
+?        Comando que será executado até que a expressão de validação torne-se falsa
+
+    }
  */
 }
