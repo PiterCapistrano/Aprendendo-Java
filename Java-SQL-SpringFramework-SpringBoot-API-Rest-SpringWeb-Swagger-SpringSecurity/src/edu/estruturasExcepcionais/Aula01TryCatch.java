86417b6266704c4215a097a9c5b0ef47e6aa2c69
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-30 08:59:41 -0300

    Aula Sobre Estrutura Excepcionais Try / Catch

---
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasExcepcionais/Aula01TryCatch.java
@@ -0,0 +1,16 @@
+package edu.estruturasExcepcionais;
+
+public class Aula01TryCatch {
+/*
+!         EXCEÇÕES
+
+    Ao executar o código Java, diferentes erros podem acontecer: erros de codificação feitos pelo programador, 
+  erros devido a entrada errada ou outros imprevistos.
+
+    Quando ocorre um erro, o Java normalmente para e gera uma mensagem de erro. O termo técnico para isso é: Java
+  lançará uma "exceção" (jogará um erro).
+
+    De forma interpretativa em Java, um erro é algo irreparável, a aplicação trava ou é encerrada drasticamente.
+  Já exceções é um fluxo inesperado da nossa aplicação, exemplo:
+*/
+}
