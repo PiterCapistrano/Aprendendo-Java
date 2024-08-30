28f32b3e436c31bd9d68465d5b78a4444a88b13f
Piter Capistrano <pitercapistrano@gmail.com>
2024-08-30 19:56:41 -0300

    Aula Sobre Estrutura Excepcionais Hierarquia das Exceções e Exceções Customizadas Completa

--- Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasExcepcionais/Aula02HierarquiaDasExcecoes.java
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/estruturasExcepcionais/Aula02HierarquiaDasExcecoes.java
@@ -29,3 +29,36 @@ public class Aula02HierarquiaDasExcecoes {
     }
   }
 }
+/*
+!           EXCEÇÕES CUSTOMIZADAS
+
+    Nós podemos criar nossas próprias exceções baseadas em regras de negócio e assim melhor direcionar quem for
+  utilizar os recursos desenvolvidos no projeto, exemplo:
+
+    - Imagina que como regra de negócio, para formatar um cep necessita sempre de ter 8 dígitos, caso contrário
+    lançará uma exceção que denominamos de CepInvalidoException.
+
+    - Primeiro criamos nossa exceção:
+
+?    public class CepInvalidoException extends Exception {}
+
+    - Em seguida criamos nosso método de formatação de cep.
+
+public class FromatadorCepExemplo {
+  public static void main(String[] args) {
+    try {
+      String cepFormatado = formatarCep("23765064");
+      System.out.println(cepFormatado);
+      } catch (CepInvalidoException e) {
+      System.out.println("O cep não corresponde com as regras de negócio");
+      }
+    }
+    static String formatarCep (String cep) throws CepInvalidoException{
+      if (cep.length() != 8) 
+        throw new CepInvalidoException();
+
+        // Simulando um cep formatado
+        return "23.765-064";
+  }
+ }
+*/
