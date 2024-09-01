5f1983a2c010c3529e1f4f0f7bd655715a9e51e5
Piter Capistrano <pitercapistrano@gmail.com>
2024-09-01 12:31:29 -0300

    Aula Sobre Debugging em Java Completa

--- Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/debuggingJava/CalculadoraDeMedias.java
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/debuggingJava/CalculadoraDeMedias.java
@@ -1,2 +1,41 @@
-package edu.debuggingJava;public class CalculadoraDeMedias {
+package edu.debuggingJava;
+
+import java.util.Scanner;
+
+public class CalculadoraDeMedias {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String [] alunos = {"Piter", "Camila", "Lucas", "Bruna"};
+
+        // Local do break point
+        //int media = calculaMediaDaTurma(alunos, scanner);
+        double media = calculaMediaDaTurma(alunos, scanner);
+
+        //System.out.printf("Média da turma %d", media);
+        System.out.printf("Média da turma %.1f", media);
+    }
+    // Para corrigir o bug todas as variáveis devem ser "double"
+    //public static int calculaMediaDaTurma(String [] alunos, Scanner scanner) {
+    public static double calculaMediaDaTurma(String [] alunos, Scanner scanner) {
+        //int soma = 0;
+        double soma = 0;
+        for (String aluno: alunos) {
+            System.out.printf("Nota do aluno %s: ", aluno);
+            //int nota = scanner.nextInt();
+            double nota = scanner.nextDouble();
+            soma += nota;
+        }
+        return soma / alunos.length;
+    }
 }
+/*
+Resultado = 7
+Resultado real = 7.5
+
+Para fazer uma depuração e analizar onde está ocorrendo o erro
+nós devemos utilizar o "break point" onde a gente acredita que o
+erro, essa exception ou inconcistência está ocorrendo.
+
+Para rodar nosso programa no modo Debug, utilizamos a opção Debug
+ao clicar no play para rodar o programa.
+ */
--- Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/debuggingJava/Main.java
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/debuggingJava/Main.java
@@ -1,2 +1,30 @@
-package edu.debuggingJava;public class Main {
+package edu.debuggingJava;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Inicio do programa no método main.");
+        a();
+        System.out.println("Finalizou o programa no método main.");
+    }
+
+    /* O método pode ou não ter o "public" antes do "satic",
+    caso não tenha por padrão o Java tratará como se fosse "public".*/
+    public static void a(){
+        System.out.println("Entrou no método a.");
+        b();
+        System.out.println("Finalizou o método a.");
+    }
+    static void b(){
+        System.out.println("Entrou no método b.");
+        for (int i = 0; i <= 4; i++) System.out.println(i);
+        c();
+        System.out.println("Finalizou o método b.");
+    }
+    static void c(){
+        System.out.println("Entrou no método c.");
+        // Comando abaixo exibe a pilha de execução.
+        // Comando "Thread.dumpStack();" é recomendado quando queremos executar uma depuração de código.
+        Thread.dumpStack();
+        System.out.println("Finalizou o método c.");
+    }
 }
