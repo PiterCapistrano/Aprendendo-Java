a5458078c7233c898d5d169410ceffa634eef965
Piter Capistrano <pitercapistrano@gmail.com>
2024-09-02 22:44:42 -0300

    Aula sobre Classes em POO

---
+++ Java-SQL-SpringFramework-SpringBoot-API-Rest-SpringWeb-Swagger-SpringSecurity/src/edu/orientacaoAObjetosJava/Classes.java
@@ -0,0 +1,15 @@
+package edu.orientacaoAObjetosJava;
+
+public class Classes {
+/*
+!       CLASSES
+
+    Toda a Estrutura de código na linguagem Java é distribuído em arquivos com extensão .java denominados de 
+  classe.
+
+    As classes existentes em nosso projeto serão composta por:
+
+?     Identificador, Características e Comportamentos.
+*/
+  
+}
